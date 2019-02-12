c01ffb8be86fb2e62e7b5d506af57687a4b1e398
Lenny Khazan <lenny.khazan@gmail.com>
2019-02-11 19:14:19 -0500

    modified implementation from https://medium.com/@ts1829/policy-gradient-reinforcement-learning-in-pytorch-df1383ea0baf?fbclid=IwAR32qy3G-xzhLvw5hYVbH5pBl9fjq9nPP89399DiOvfCujrWOFPoljDbrVw

---
+++ run.py
@@ -0,0 +1,116 @@
+import torch
+import numpy as np
+import Environment
+
+INPUT_SIZE = 2
+STACK_SIZE = 2
+ACTIONS = 2
+GAMMA = 0.99
+HIDDEN_SIZE = 64
+LEARNING_RATE = 0.01
+NUM_EPISODES = 1000
+
+
+class Policy(torch.nn.Module):
+  def __init__(self):
+    super(Policy, self).__init__()
+    self.state_space = INPUT_SIZE + STACK_SIZE
+    self.action_space = ACTIONS
+
+    self.l1 = torch.nn.Linear(self.state_space, HIDDEN_SIZE, bias=False)
+    self.l2 = torch.nn.Linear(HIDDEN_SIZE, self.action_space, bias=False)
+
+    self.gamma = GAMMA
+
+    # Episode policy and reward history
+    self.policy_history = torch.autograd.Variable(torch.Tensor())
+    self.reward_episode = []
+
+    # Overall reward and loss history
+    self.reward_history = []
+    self.loss_history = []
+
+  def forward(self, x):
+    model = torch.nn.Sequential(
+        self.l1,
+        torch.nn.Dropout(p=0.6),
+        torch.nn.ReLU(),
+        self.l2,
+        torch.nn.Softmax(dim=ACTIONS)
+    )
+    return model(x)
+
+
+def select_action(policy, state):
+    # Select an action (0 or 1) by running policy model and choosing based on the probabilities in state
+  state = torch.from_numpy(state).type(torch.FloatTensor)
+  state = policy(torch.autograd.Variable(state))
+  c = torch.distributions.distribution.Categorical(state)
+  action = c.sample()
+
+  # Add log probability of our chosen action to our history
+  if policy.policy_history.dim() != 0:
+    policy.policy_history = torch.cat(
+        [policy.policy_history, c.log_prob(action)])
+  else:
+    policy.policy_history = (c.log_prob(action))
+  return action
+
+
+def update_policy(policy, optimizer):
+  R = 0
+  rewards = []
+
+  # Discount future rewards back to the present using gamma
+  for r in policy.reward_episode[::-1]:
+    R = r + policy.gamma * R
+    rewards.insert(0, R)
+
+  # Scale rewards
+  rewards = torch.FloatTensor(rewards)
+  rewards = (rewards - rewards.mean()) / \
+      (rewards.std() + np.finfo(np.float32).eps)
+
+  # Calculate loss
+  loss = (torch.sum(torch.mul(policy.policy_history,
+                              torch.autograd.Variable(rewards)).mul(-1), -1))
+
+  # Update network weights
+  optimizer.zero_grad()
+  loss.backward()
+  optimizer.step()
+
+  # Save and intialize episode history counters
+  policy.loss_history.append(loss.data[0])
+  policy.reward_history.append(np.sum(policy.reward_episode))
+  policy.policy_history = torch.autograd.Variable(torch.Tensor())
+  policy.reward_episode = []
+
+
+def main():
+  policy = Policy()
+  optimizer = torch.optim.Adam(policy.parameters(), lr=LEARNING_RATE)
+  for episode in range(NUM_EPISODES):
+    env = Environment()
+
+    while not env.is_done():
+      state = env.observe_next_state()
+      action = select_action(policy, state)
+      reward = 0
+      if action == 0:
+        reward = env.push()
+      elif action == 1:
+        reward = env.pop()
+
+      # Save reward
+      policy.reward_episode.append(reward)
+
+    update_policy(policy, optimizer)
+    if episode % 50 == 0:
+      print('Episode {}\tAverage reward: {:.2f}'.format(
+          episode, sum(policy.reward_history) / len(policy.reward_history)))
+      policy.reward_history = []
+
+
+if __name__ == '__main__':
+  main()
